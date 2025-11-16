740c1b9b664d0af74389409d323aa396efa2070e
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-11-16 18:50:01 +0100

    feat: Create DataStore Interface and move old code to new class

--- src/DataStore.java
+++ src/DataStore.java
@@ -1,16 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
-public class DataStore {
-    private Map<Integer,String> store= new HashMap<>();
 
-    public void write(int index, String data){
-        store.put(index,data);
-    }
-    public String read(int index) throws NoSuchElementException{
-        if(store.containsKey(index)){
-            return store.get(index);
-        }
-        throw new NoSuchElementException("No value at index "+index);
-    }
+public interface DataStore {
+    public void write(int index, String data);
+    public String read(int index) throws NoSuchElementException;
 }
