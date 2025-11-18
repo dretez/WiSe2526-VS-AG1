2dbcc24a653c05237b870a68af13f4513d24374f
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-11-18 16:43:21 +0100

    feat(JSONReader): Update factory

--- src/JSONReader.java
+++ src/JSONReader.java
@@ -1,4 +1,3 @@
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -8,15 +7,9 @@ public class JSONReader {
         this.json = json;
     }
 
-    public static JSONReader fromFile(String filename) {
-        try (var in = JSONReader.class.getResourceAsStream(filename)) {
-            if (in == null) return null;
-            String json = new String(in.readAllBytes());
-            Object parsed = JSONParser.parse(json);
-            return new JSONReader(parsed);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static JSONReader fromString(String json) {
+        Object parsed = JSONParser.parse(json);
+        return new JSONReader(parsed);
     }
 
     public Object get(String keyPath) {
