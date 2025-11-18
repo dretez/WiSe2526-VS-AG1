c61c442245efc877c48c549cfd6be18d1d6e6f84
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-11-18 13:31:30 +0100

    feat: Setup Main to run local and RPC calls for comparisson

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
+        // local call
+        LocalDataStore lds = new LocalDataStore();
+        lds.write(1, "Hallo Welt");
+        try {
+            String val = lds.read(1);
+            System.out.println(val);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        // remote call
         Client client = new Client();
         try {
             client.connect();
-            client.write(10,"hallo welt");
+
+            client.write(1,"Hallo Welt");
+            String val = client.read(1);
+            System.out.println(val);
+
             client.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
         }
     }
 }
